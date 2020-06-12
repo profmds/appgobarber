4ee50db095d19e7289b23a8a37a7cbb60b745d9c
Marcelo dos Santos <profmds@gmail.com>
2020-06-12 01:19:16 -0300

    Create Appointment commit

--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,37 +1,39 @@
 import styled from 'styled-components/native';
 import { Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
-  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
+  padding: 0 24px;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 32px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
-  margin: 64px 0 24px;
+  margin-top: 48px;
+  text-align: center;
 `;
 
-export const BackToSignIn = styled.TouchableOpacity`
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background: #312e38;
-  border-top-width: 1px;
-  border-top-color: #232129;
-  padding: 16px 0;
+export const Description = styled.Text`
+  font-size: 18px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  margin-top: 16px;
+`;
 
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  padding: 12px 24px;
   align-items: center;
   justify-content: center;
-  flex-direction: row;
+  border-radius: 10px;
+  margin-top: 24px;
 `;
-export const BackToSignInText = styled.Text`
-  color: #fff;
+export const OkButtonText = styled.Text`
+  color: #312e38;
   font-size: 18px;
-  font-family: 'RobotoSlab-Regular';
-  margin-left: 16px;
+  font-family: 'RobotoSlab-Medium';
 `;
